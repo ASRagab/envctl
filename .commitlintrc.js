d512e430ec1fc493f07401609db9bfd864711619
Ahmad Ragab <ahmad.ragab@joinhandshake.com>
2025-06-26 05:07:07 -0400

    fix(ci): release

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -8,5 +8,9 @@ module.exports = {
     ],
     'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
     'subject-max-length': [2, 'always', 100],
+    // Allow longer lines in release commits (semantic-release generates long changelog entries)
+    'body-max-line-length': [0, 'always', 100], // Disable body line length limit
   },
+  // Ignore release commits made by semantic-release
+  ignores: [(message) => /^chore\(release\):/.test(message)],
 }
